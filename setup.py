450f42d583de30bfa36f1f4dcaa90fd5c7d9f784
Max Gribennikov <maxim.gribennikov@quantumsoft.ru>
2020-01-23 20:35:30 +0700

    Update setup.py info

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup
 
+PACKAGE = 'allure-commandline-python'
+VERSION = '0.0.3'
+
+install_requires = [
+    'requests>=2.22.0'
+]
+
 setup(
-    name='allure-commandline-python',
-    version='0.0.2',
+    name=PACKAGE,
+    version=VERSION,
     packages=['allure_commandline_python'],
     package_dir={'allure_commandline_python': 'src'},
+    install_requires=install_requires,
     url='https://github.com/grmmvv/allure-commandline-python',
     license='unlicense',
     author='Max Gribennikov',
